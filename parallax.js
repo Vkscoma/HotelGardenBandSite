4f191ebc843c7e7c7a3e8e0d62eaf35b02312f79
Vkscoma <vkscoma@gmail.com>
2023-02-03 20:14:18 -0800

    Final final touches

---
+++ parallax.js
@@ -0,0 +1,17 @@
+const h1 = document.getElementById("h1"),
+  banner = document.getElementById("banner");
+
+const onScroll = (event) => {
+  const scrollPosition = event.target.scrollingElement.scrollTop;
+  if (scrollPosition > 150) {
+    // banner.style.backgroundSize = "100%";
+    h1.style.scale = "0.9";
+    h1.style.opacity = 0.5;
+  } else {
+    // banner.style.backgroundSize = "120%";
+    h1.style.scale = 1;
+    h1.style.opacity = 1;
+  }
+};
+
+document.addEventListener("scroll", onScroll);
